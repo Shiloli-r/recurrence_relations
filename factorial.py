80d79abbeb761661c53d2e231498176aaca58845
Shiloli-r <45619599+Shiloli-r@users.noreply.github.com>
2020-12-08 19:42:11 +0300

    Plot of running time as input size grows

--- factorial.py
+++ factorial.py
@@ -1,4 +1,5 @@
 import timeit
+import matplotlib.pyplot as plt
 
 
 def fact(number):
@@ -9,11 +10,19 @@ def fact(number):
 
 
 def main():
-    start = timeit.default_timer()
-    ans = fact(10)
-    print(ans)
-    running_time = timeit.default_timer() - start
-    print("Running Time in Seconds: %.10f" % running_time)
+    input_size = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    time_taken = []
+    for input_ in input_size:
+        start = timeit.default_timer()
+        ans = fact(input_)
+        running_time = timeit.default_timer() - start
+        time_taken.append(running_time)
+        print(ans)
+        print("Input Size {} \nRunning Time in Seconds: %.10f \n\n".format(input_) % running_time)
+    plt.plot(time_taken, input_size)
+    plt.xlabel("Time Taken")
+    plt.ylabel("Input size")
+    plt.show()
 
 
 if __name__ == '__main__':
